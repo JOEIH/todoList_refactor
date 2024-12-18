ab3fc5dd1b93ea2c24339a8da7ed3fd3cb6e0f3b
JOEIH <joh73459@gmail.com>
2024-12-19 02:41:48 +0900

    ✨ Feat : todo 추가 기능 구현 #3

--- src/addTodo.ts
+++ src/addTodo.ts
@@ -1,4 +1,39 @@
-let todoInput = document.querySelector('#add-todo');
-let addTodoButton = document.querySelector('#add-todo-button');
-let todoBox = document.querySelector('#todos');
+const todoForm = document.getElementById('todo-form');
+const todoInput = document.querySelector<HTMLInputElement>('#add-todo');
+let todoBox = document.querySelector<HTMLDivElement>('#todos');
 
+const addNewTodo = () => {
+  if (todoInput && todoInput.value !== '') {
+    // 투두 하나의 컨테이너 생성
+    const newTodoContainer = document.createElement('label');
+    newTodoContainer.className = 'todo-item'
+
+    // 체크박스 생성
+    const todoCheckbox = document.createElement('input');
+    todoCheckbox.setAttribute('type', 'checkbox');
+    todoCheckbox.setAttribute('id', 'todo-check')
+
+    // 커스텀 체크박스용 span 생성
+    const customCheckbox = document.createElement('span');
+    customCheckbox.setAttribute('id', 'checkbox-icon');
+
+    // 텍스트 들어갈 span 생성
+    const newTodoText = document.createElement('span');
+    newTodoText.setAttribute('id', 'todo-text')
+    newTodoText.textContent += todoInput.value;
+
+    // 투두 컨테이너 안에 체크박스, 텍스트 div 추가
+    newTodoContainer.appendChild(todoCheckbox);
+    newTodoContainer.appendChild(customCheckbox);
+    newTodoContainer.appendChild(newTodoText);
+
+    // 투두리스트들을 div에 추가
+    todoBox?.appendChild(newTodoContainer);
+    todoInput.value = '';
+  }
+}
+
+todoForm?.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addNewTodo();
+})
