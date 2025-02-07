aaf0d6c5ecf085c29812d121f45dfb4c93979c68
JOEIH <joh73459@gmail.com>
2025-02-07 14:35:59 +0900

    💄 Design : input 자동완성 시 배경색상 삭제

--- src/addTodo.ts
+++ src/addTodo.ts
@@ -3,33 +3,40 @@ const todoInput = document.querySelector<HTMLInputElement>('#add-todo');
 let todoBox = document.querySelector<HTMLDivElement>('#todos');
 
 const addNewTodo = () => {
-  if (todoInput && todoInput.value !== '') {
-    // 투두 하나의 컨테이너 생성
-    const newTodoContainer = document.createElement('label');
-    newTodoContainer.className = 'todo-item'
+  if (todoInput) {
+    if (!todoInput.value.trim()) {
+      alert('공백으로만 이루어진 항목은 입력할 수 없습니다.')
+      todoInput.value = ''
+    }
 
-    // 체크박스 생성
-    const todoCheckbox = document.createElement('input');
-    todoCheckbox.setAttribute('type', 'checkbox');
-    todoCheckbox.setAttribute('id', 'todo-check')
+    if (todoInput.value.trim()) {
+      // 투두 하나의 컨테이너 생성
+      const newTodoContainer = document.createElement('label');
+      newTodoContainer.className = 'todo-item'
 
-    // 커스텀 체크박스용 span 생성
-    const customCheckbox = document.createElement('span');
-    customCheckbox.setAttribute('id', 'checkbox-icon');
+      // 체크박스 생성
+      const todoCheckbox = document.createElement('input');
+      todoCheckbox.setAttribute('type', 'checkbox');
+      todoCheckbox.setAttribute('id', 'todo-check')
 
-    // 텍스트 들어갈 span 생성
-    const newTodoText = document.createElement('span');
-    newTodoText.setAttribute('id', 'todo-text')
-    newTodoText.textContent += todoInput.value;
+      // 커스텀 체크박스용 span 생성
+      const customCheckbox = document.createElement('span');
+      customCheckbox.setAttribute('id', 'checkbox-icon');
 
-    // 투두 컨테이너 안에 체크박스, 텍스트 div 추가
-    newTodoContainer.appendChild(todoCheckbox);
-    newTodoContainer.appendChild(customCheckbox);
-    newTodoContainer.appendChild(newTodoText);
+      // 텍스트 들어갈 span 생성
+      const newTodoText = document.createElement('span');
+      newTodoText.setAttribute('id', 'todo-text')
+      newTodoText.textContent += todoInput.value;
 
-    // 투두리스트들을 div에 추가
-    todoBox?.appendChild(newTodoContainer);
-    todoInput.value = '';
+      // 투두 컨테이너 안에 체크박스, 텍스트 div 추가
+      newTodoContainer.appendChild(todoCheckbox);
+      newTodoContainer.appendChild(customCheckbox);
+      newTodoContainer.appendChild(newTodoText);
+
+      // 투두리스트들을 div에 추가
+      todoBox?.appendChild(newTodoContainer);
+      todoInput.value = '';
+    }
   }
 }
 
