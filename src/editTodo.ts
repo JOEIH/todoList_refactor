20b4ad6597dd7d6236131339170238e35fb69e03
JOEIH <joh73459@gmail.com>
2025-02-21 20:30:56 +0900

    ♻️ Refactor : 투두 아이템 생성 방식 insertAdjacentHTML로 변경
    나중에 보안 문제도 생각하기...

---
+++ src/editTodo.ts
@@ -0,0 +1,6 @@
+// const editButton = document.getElementById('edit-button');
+// const todoText = document.querySelector<HTMLSpanElement>('todo-text');
+
+// export const editTodo = () => {
+
+// }
