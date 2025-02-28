1c530e5e8da71b7db7a9b6e542801ede62871276
JOEIH <joh73459@gmail.com>
2025-03-01 01:33:56 +0900

    🐛 Fix : 해당하는 요소만 삭제되도록 버그 수정

--- client/src/deleteTodo.ts
+++ client/src/deleteTodo.ts
@@ -2,13 +2,17 @@ import { deleteTodo } from "./apis/apis";
 
 document.addEventListener('click', (e) => {
   const target = e.target as HTMLElement;
-  const targetTodo = target.closest('.todo-item');
 
-  if (targetTodo) {
-    let answer = confirm('정말 삭제하시겠습니까?');
-
-    if (answer) {
-      deleteTodo(targetTodo.id);
-    } 
+  if (target.classList.contains('delete-button')) {
+    const targetTodo = target.closest('.todo-item');
+    
+    if (targetTodo) {
+      const id = targetTodo.id;
+      let answer = confirm('정말 삭제하시겠습니까?');
+  
+      if (answer) {
+        deleteTodo(id);
+      } 
+    }
   }
 });
