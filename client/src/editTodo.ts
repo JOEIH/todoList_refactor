a209c19b08fc99f711c2ea566a92ac12601509b2
JOEIH <joh73459@gmail.com>
2025-03-07 19:30:07 +0900

    ✨ Feat : 수정 기능 추가 #3

--- client/src/editTodo.ts
+++ client/src/editTodo.ts
@@ -1,11 +1,37 @@
-const todoText = document.querySelector<HTMLInputElement>('#todo-text');
-const editButton = document.querySelector<HTMLButtonElement>('#edit-button');
+import { editTodo } from "./apis/apis";
 
-export const editTodo = (baseText: string) => {
-  if (todoText?.value && editButton) {
-    editButton.style.display = 'none';
+document.addEventListener('click', (e) => {
+  const target = e.target as HTMLElement;
 
-    todoText.readOnly = false;
-    todoText.value = baseText;
+  if (target.classList.contains('edit-button')) {
+    const targetTodo = target.closest('.todo-item');
+
+    if (targetTodo) {
+      const id = targetTodo.id;
+      const deleteButton = targetTodo.querySelector('.delete-button') as HTMLButtonElement;
+      const targetInput = targetTodo.querySelector('.todo-text') as HTMLInputElement;
+      const editButton = targetTodo.querySelector('.edit-button') as HTMLButtonElement;
+      const checkbox = targetTodo.querySelector('.todo-check') as HTMLInputElement;
+  
+      // 수정 완료 버튼 생성
+      const finishButton = document.createElement('button');
+      finishButton.setAttribute('class', 'finish-button');
+      finishButton.textContent = "완료";
+      
+      // 수정 버튼 클릭 시 input focus와 완료 버튼 나타나도록 설정
+      deleteButton.style.display = "none";
+      editButton.style.display = "none";
+      targetTodo.appendChild(finishButton);
+      targetInput.readOnly = false;
+      targetInput.focus();
+  
+      // 완료 버튼 누르면 수정 완료
+      finishButton.addEventListener('click', () => {
+        finishButton.style.display = "none";
+        deleteButton.style.display = "inline-block";
+        editButton.style.display = "inline-block";
+        editTodo(id, checkbox.checked, targetInput.value);
+      })
+    }
   }
-}
+})
